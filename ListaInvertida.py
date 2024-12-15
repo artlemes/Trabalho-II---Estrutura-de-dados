f2a7786a5adcce365687b2d98e8c50550849288c
LucasG0es <lucasrodriguesgoes@outlook.com>
2024-12-15 20:18:34 -0300

    Create ListaInvertida.py

---
+++ ListaInvertida.py
@@ -0,0 +1,27 @@
+
+
+class ListaInvertida:
+    def __init__(self):
+        self.__dict = {}
+
+    def incluir(self, categoria: str, id: int):
+        if categoria not in dict:
+            self.__dict[categoria] = []
+        
+        if id not in self.__dict[categoria]:
+            self.__dict[categoria].append(id)
+
+    def remover(self, categoria: str, id: int):
+        if categoria not in dict:
+            return
+
+        if id not in self.__dict[categoria]:
+            return
+
+        self.__dict[categoria].remove(id)
+    
+    def buscaPorCategoria(self, categoria):
+        if categoria not in dict:
+            return []
+        
+        return self.__dict[categoria]
