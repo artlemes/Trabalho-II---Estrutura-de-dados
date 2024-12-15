ab746127aa303518c979442044eceb350ff956a6
LucasG0es <lucasrodriguesgoes@outlook.com>
2024-12-15 20:25:04 -0300

    Update ListaInvertida.py

--- ListaInvertida.py
+++ ListaInvertida.py
@@ -4,14 +4,14 @@ class ListaInvertida:
     def __init__(self):
         self.__dict = {}
 
-    def incluir(self, categoria: str, id: int):
+    def incluir(self, categoria, id: int):
         if categoria not in dict:
             self.__dict[categoria] = []
         
         if id not in self.__dict[categoria]:
             self.__dict[categoria].append(id)
 
-    def remover(self, categoria: str, id: int):
+    def remover(self, categoria, id: int):
         if categoria not in dict:
             return
 
