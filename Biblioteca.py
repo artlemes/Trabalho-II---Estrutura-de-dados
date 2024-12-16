3f4fd5f51c968bb601a4fa365bc3488c7ce5bb7b
LucasG0es <lucasrodriguesgoes@outlook.com>
2024-12-15 21:20:37 -0300

    Update Biblioteca.py

--- Biblioteca.py
+++ Biblioteca.py
@@ -21,8 +21,8 @@ class Biblioteca():
         
         self.__estante[id] = livroNovo
 
-        self.__listaInvertidaAutor.incluir(livroNovo.getAutor(), id)
         self.__listaInvertidaGenero.incluir(livroNovo.getGenero(), id)
+        self.__listaInvertidaAutor.incluir(livroNovo.getAutor(), id)
         self.__listaInvertidaPreco.incluir(livroNovo.getPreco(), id)
 
     def buscarPorId(self, id):
@@ -38,11 +38,39 @@ class Biblioteca():
 
         livro = self.buscarPorId(id)
 
-        self.__listaInvertidaAutor.remover(livro.getAutor(), id)
         self.__listaInvertidaGenero.remover(livro.getGenero(), id)
+        self.__listaInvertidaAutor.remover(livro.getAutor(), id)
         self.__listaInvertidaPreco.remover(livro.getPreco(), id)
 
         self.__estante.pop(id)
+
+    def buscaPorCategoria(self, genero = None, autor = None, precoCompLogica = None, precoValor = None):
+
+        if precoCompLogica == None and precoValor != None:
+            precoCompLogica = "="
+
+        listaRetorno = set()
+
+        listaDeIdPorGenero = self.__listaInvertidaGenero.buscaPorCategoria(genero)
+        listaDeIdPorAutor = self.__listaInvertidaGenero.buscaPorCategoria(autor)
+        listaDeIdPorPreco = self.__listaInvertidaGenero.buscaPorCategoria(precoCompLogica, precoValor)
+
+        if genero != None:
+            listaRetorno = listaRetorno | set(listaDeIdPorGenero)
         
+        if autor != None:
+            if genero != None:
+                listaRetorno = listaRetorno & set(listaDeIdPorAutor)
+            else:
+                listaRetorno = listaRetorno | set(listaDeIdPorAutor)
+        
+        if precoValor != None:
+            if genero != None or autor != None:
+                listaRetorno = listaRetorno & set(listaDeIdPorPreco)
+            else:
+                listaRetorno = listaRetorno | set(listaDeIdPorPreco)
+        
+        return list(listaRetorno)
+
     def exibirTodosDados(self):
         print(self.__estante)
